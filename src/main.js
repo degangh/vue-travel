c23d395945a9657382fa2d03bd2b159e5b5eec54
unknown <nicolash@fusion-solutions.com.au>
2019-02-05 15:40:53 +1030

    install and import ant-design

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+
+Vue.use(Antd)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
