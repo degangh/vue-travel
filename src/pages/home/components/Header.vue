a7181b1c44c70f8b076e423f74694e812ed6733d
unknown <nicolash@fusion-solutions.com.au>
2019-02-05 11:22:56 +1030

    header layout design

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,42 @@
+<template class="home">
+<div class="header">
+<div class="header-left">Back</div>
+<div class="header-input">Search Keyword</div>
+<div class="header-right">City</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus"  scoped>
+.header
+    display: flex
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+
+    .header-left
+        width: .64rem
+        float: left
+
+    .header-input
+        flex: 1
+        height: .64rem
+        line-height: .64rem
+        margin-top: .12rem
+        margin-left: .2rem
+        background: #fff
+        border-radius: .1rem
+        color: #ccc
+
+    .header-right
+        text-align: center
+        width: 1.24rem
+        float: right
+
+</style>
