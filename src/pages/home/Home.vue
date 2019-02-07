dd372a1d6b2ac3b167be22003a3c7e66a5a26082
unknown <nicolash@fusion-solutions.com.au>
2019-02-07 11:50:07 +1030

    Add icons area

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
 <home-header></home-header>
 
 <home-carousel></home-carousel>
+<home-icons></home-icons>
 </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeCarousel from './components/Carousel'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
-    HomeCarousel: HomeCarousel
+    HomeCarousel: HomeCarousel,
+    HomeIcons: HomeIcons
   }
 }
 </script>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,118 @@
+<template>
+<div class= "icons">
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+    <div class="icon">
+        <div class="icon-content">
+            <a-icon type="dollar" class="icon-content-inner"/>
+        </div>
+        <p class="icon-desc">Logo</p>    
+    </div>
+
+
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HomeIcons',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .icons
+        overflow hidden
+        
+        height: 0
+        padding-bottom: 50%
+
+        .icon
+            position: relative
+            display:inline-block
+            width:24%
+            height: 0
+            padding-bottom:25%
+
+            .icon-content
+                position: absolute
+                top:0
+                left:0
+                right:0
+                bottom: .44rem
+                box-sizing: border-box
+                padding: .1rem
+                .icon-content-inner
+                    display:block
+                    margin: 0 auto
+                    height:100%
+                    font-size: .8rem
+                    text-align:center
+                    color: #c0c0c0
+
+            .icon-desc
+                position: absolute
+                left: 0
+                right: 0
+                bottom: 0
+                line-height: .44rem
+                height: .44rem
+                color: #333
+                text-align:center
+                    
+
+                    
+
+
+                
+</style>
