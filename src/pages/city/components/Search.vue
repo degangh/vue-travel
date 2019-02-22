40b6a218f91ff51f8c34a786200d43ad4d86f480
nicolas <degang.huang@gmail.com>
2019-02-22 21:46:59 +1030

    Add search input

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" class="search-input" placeholder="Search destination">
+    </div>
+</template>
+
+<script>
+export default{
+  name: 'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/variables.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      height: .62rem
+      line-height: .62rem
+      width: 100%
+      text-align: center
+      border-radius: 2px
+      color: #666
+      box-sizing: border-box
+      padding: 0 .1rem
+</style>
