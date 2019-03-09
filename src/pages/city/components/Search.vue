893e1773e479df7588f173192609d19875f17aaf
nicolas <degang.huang@gmail.com>
2019-03-09 22:55:08 +1030

    Add search item

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,17 +1,19 @@
 <template>
   <div>  
-    <div class="search">
+    <div class="search" >
         <input type="text" class="search-input" v-model = "keyword" placeholder="Search destination">
     </div>
-    <div class="search-content">
+    <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li v-for = "item in list" :key = "item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-for = "item in list" :key = "item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">No places found</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default{
   name: 'CitySearch',
   props: {
@@ -24,11 +26,24 @@ export default{
       timer: null
     }
   },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+
+  computed: {
+    hasNoData () {
+      return !this.list.length 
+    }
+  },
   
   watch: {
     keyword () {
       if (this.timer) {
         clearTimeout(this.timer)
+      }
+      if (!this.keyword){
+        this.list = []
+        return
       }
       this.timer = setTimeout(() => {
         const result = []
@@ -70,4 +85,10 @@ export default{
     left: 0
     right: 0
     bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
