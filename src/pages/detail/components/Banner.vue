a486b951f3178e118e9b504d96361540a7618ace
nicolas <degang.huang@gmail.com>
2019-03-15 23:17:35 +1030

    create new page detail tempalte

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="banner">
+    <img src="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-540x360/06/90/de/bb.jpg" class="banner-img"/>    
+    <div class="banner-info">
+        <div></div>
+        <div></div>
+    </div>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'DetailBanner'
+}
+</script>
+
+<style lang = "stylus" scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+
+</style>
